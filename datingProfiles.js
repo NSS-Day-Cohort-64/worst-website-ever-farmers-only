8404366ab0b7ac911b1a47220a8385c4fe941cc0
Sam Thrasher <samthrasher9@gmail.com>
2023-04-28 10:44:16 -0500

    created a dating profile file in JS

---
+++ datingProfiles.js
@@ -0,0 +1,16 @@
+export const datingProfiles = [
+ {
+        name: "abc123" ,
+        photo: "abc123",
+        theories: "abc123",
+        favoriteConspiracyQuote: "abc123",
+        lookingFor: "abc123",
+    },
+    {
+        name: "abc123" ,
+        photo: "abc123",
+        theories: "abc123",
+        favoriteConspiracyQuote: "abc123",
+        lookingFor: "abc123",
+    }
+]
