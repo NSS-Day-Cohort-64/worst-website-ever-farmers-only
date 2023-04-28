50d0329d2b72aee7e16fdabd224a83a22b53b83c
Sam Thrasher <samthrasher9@gmail.com>
2023-04-28 11:43:38 -0500

    added our first client

--- datingProfiles.js
+++ datingProfiles.js
@@ -1,5 +1,19 @@
 export const datingProfiles = [
- {
+    {
+        name: "Steve" ,
+        photo: "https://nashvillesoftwareschool.com/images/instructors/steve.jpg",
+        theories: "The illuminati is real, but they only have control of the super bowl halftime show and certain stoplights in the world.",
+        favoriteConspiracyQuote: `"I am not paranoid, they are just watching me." - Unknown` ,
+        lookingFor: "Someone who understands gitHub",
+    },
+    {
+        name: "abc123" ,
+        photo: "abc123",
+        theories: "abc123",
+        favoriteConspiracyQuote: "abc123",
+        lookingFor: "abc123",
+    },
+    {
         name: "abc123" ,
         photo: "abc123",
         theories: "abc123",
@@ -14,7 +28,7 @@ export const datingProfiles = [
         lookingFor: "abc123",
     },
     {
-        name: "abc123" ,
+        name: "Mr. Rainbow Unicorn Sparkly Lights" ,
         photo: "abc123",
         theories: "abc123",
         favoriteConspiracyQuote: "abc123",
