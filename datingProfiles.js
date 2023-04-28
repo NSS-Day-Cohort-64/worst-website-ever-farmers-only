677feb731d71138fa8301597f3b687266237f04f
Sam Thrasher <samthrasher9@gmail.com>
2023-04-28 13:19:54 -0500

    finalized an order of our dating profiles

--- datingProfiles.js
+++ datingProfiles.js
@@ -1,10 +1,24 @@
 export const datingProfiles = [
     {
+        name: "Farmer Cindy" ,
+        photo: "https://previews.123rf.com/images/6okean/6okean1706/6okean170600106/80261134-funny-picture-a-beautiful-young-girl-farmer-with-in-hat.jpg",
+        theories: "The reason aliens abduct cows is because they're trying to create the perfect milkshake recipe.",
+        favoriteConspiracyQuote: `"I'm not paranoid, they're just watching me." - John Lennon`,
+        lookingFor: "Anyone honestly. I am very lonely, please help me.",
+    },
+    {
         name: "Steve" ,
         photo: "https://nashvillesoftwareschool.com/images/instructors/steve.jpg",
         theories: "The illuminati is real, but they only have control of the super bowl halftime show and certain stoplights in the world.",
         favoriteConspiracyQuote: `"I am not paranoid, they are just watching me." - Unknown` ,
         lookingFor: "Someone who understands gitHub",
+    },
+    {
+        name: "Farm Hand Phil" ,
+        photo: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fdesignyoutrust.com%2Fwp-content%2Fuploads%2F2018%2F12%2Firish-farmer-calendar1.jpg&tbnid=R7qwAKSycTkjMM&vet=12ahUKEwiw3-u2lc3-AhXfjoQIHbd0Am0QMyglegUIARCrAg..i&imgrefurl=https%3A%2F%2Fdesignyoutrust.com%2F2018%2F12%2F2019-irish-farmer-calendar-is-here%2F&docid=ucd_Ex6sQtCbqM&w=660&h=495&q=funny%20farmer%20pictures&ved=2ahUKEwiw3-u2lc3-AhXfjoQIHbd0Am0QMyglegUIARCrAg",
+        theories: "The people in charge have all been replaced by lizards.",
+        favoriteConspiracyQuote: `"People say I'm crazy for believing in lizard people, but have you seen Mark Zuckerberg's eyes?" - Steve`,
+        lookingFor: "A non-lizard person.",
     },
     {
         name: "Greg" ,
@@ -21,24 +35,10 @@ export const datingProfiles = [
         lookingFor: "A good ole farm hand who loves to build legos.",
     },
     {
-        name: "Farmer Cindy" ,
-        photo: "https://previews.123rf.com/images/6okean/6okean1706/6okean170600106/80261134-funny-picture-a-beautiful-young-girl-farmer-with-in-hat.jpg",
-        theories: "The reason aliens abduct cows is because they're trying to create the perfect milkshake recipe.",
-        favoriteConspiracyQuote: `"I'm not paranoid, they're just watching me." - John Lennon`,
-        lookingFor: "Anyone honestly. I am very lonely, please help me.",
-    },
-    {
         name: "Mr. Rainbow Unicorn Sparkly Lights" ,
         photo: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fthumbs.dreamstime.com%2Fz%2Ffunny-farmer-man-shovel-rake-open-mouthed-expressive-portrait-79253819.jpg&tbnid=CtwiFjdfPSYjsM&vet=12ahUKEwiw3-u2lc3-AhXfjoQIHbd0Am0QMygGegUIARDZAQ..i&imgrefurl=https%3A%2F%2Fwww.dreamstime.com%2Fstock-photo-funny-farmer-man-shovel-rake-open-mouthed-expressive-portrait-image79253819&docid=3tKfUPWKHvOr-M&w=1300&h=957&q=funny%20farmer%20pictures&ved=2ahUKEwiw3-u2lc3-AhXfjoQIHbd0Am0QMygGegUIARDZAQ",
         theories: "The real reason the Titanic sank is because it hit an iceberg made of government secrets.",
         favoriteConspiracyQuote: `"I don't trust anyone who says they don't believe in conspiracy theories. It's like saying you don't believe in unicorns." - Neil deGrasse Tyson`,
         lookingFor: "Someone to love me as much as Casey loves his cat.",
-    },
-    {
-        name: "Farm Hand Phil" ,
-        photo: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fdesignyoutrust.com%2Fwp-content%2Fuploads%2F2018%2F12%2Firish-farmer-calendar1.jpg&tbnid=R7qwAKSycTkjMM&vet=12ahUKEwiw3-u2lc3-AhXfjoQIHbd0Am0QMyglegUIARCrAg..i&imgrefurl=https%3A%2F%2Fdesignyoutrust.com%2F2018%2F12%2F2019-irish-farmer-calendar-is-here%2F&docid=ucd_Ex6sQtCbqM&w=660&h=495&q=funny%20farmer%20pictures&ved=2ahUKEwiw3-u2lc3-AhXfjoQIHbd0Am0QMyglegUIARCrAg",
-        theories: "The people in charge have all been replaced by lizards.",
-        favoriteConspiracyQuote: `"People say I'm crazy for believing in lizard people, but have you seen Mark Zuckerberg's eyes?" - Steve`,
-        lookingFor: "A non-lizard person.",
     }
 ]
