540f14bcfd26ad9c28989579d8d15b2e6a9549c6
Sam Thrasher <samthrasher9@gmail.com>
2023-04-28 14:13:42 -0500

    Our first User

--- datingProfiles.js
+++ datingProfiles.js
@@ -6,39 +6,4 @@ export const datingProfiles = [
         favoriteConspiracyQuote: `"I'm not paranoid, they're just watching me." - John Lennon`,
         lookingFor: "Anyone honestly. I am very lonely, please help me.",
     },
-    {
-        name: "Steve" ,
-        photo: "https://nashvillesoftwareschool.com/images/instructors/steve.jpg",
-        theories: "The illuminati is real, but they only have control of the super bowl halftime show and certain stoplights in the world.",
-        favoriteConspiracyQuote: `"I am not paranoid, they are just watching me." - Unknown` ,
-        lookingFor: "Someone who understands gitHub",
-    },
-    {
-        name: "Farm Hand Phil" ,
-        photo: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fdesignyoutrust.com%2Fwp-content%2Fuploads%2F2018%2F12%2Firish-farmer-calendar1.jpg&tbnid=R7qwAKSycTkjMM&vet=12ahUKEwiw3-u2lc3-AhXfjoQIHbd0Am0QMyglegUIARCrAg..i&imgrefurl=https%3A%2F%2Fdesignyoutrust.com%2F2018%2F12%2F2019-irish-farmer-calendar-is-here%2F&docid=ucd_Ex6sQtCbqM&w=660&h=495&q=funny%20farmer%20pictures&ved=2ahUKEwiw3-u2lc3-AhXfjoQIHbd0Am0QMyglegUIARCrAg",
-        theories: "The people in charge have all been replaced by lizards.",
-        favoriteConspiracyQuote: `"People say I'm crazy for believing in lizard people, but have you seen Mark Zuckerberg's eyes?" - Steve`,
-        lookingFor: "A non-lizard person.",
-    },
-    {
-        name: "Greg" ,
-        photo: "https://cohort7.nashvillesoftwareschool.com/images/GregKorteBW.jpg",
-        theories: "The moon is made of cheese, and NASA has been covering it up for years. Birds are actually government drones used to spy on us, that's why I only shower in underground bunkers. ",
-        favoriteConspiracyQuote: `"The pyramids were actually built by ancient aliens who were really into geometry." - Confucius`,
-        lookingFor: "Someone who can make a cool sound effect when I leave rooms in Zoom.",
-    },
-    {
-        name: "Tiana" ,
-        photo: "https://media.licdn.com/dms/image/C5603AQFkRWUAzFZUeA/profile-displayphoto-shrink_400_400/0/1640138554353?e=1687996800&v=beta&t=6gtN_Vo4I6WwM2iMySYkYjmzzkRjBGo5tTEPYdIdyTc",
-        theories: "The earth is actually a giant cosmic snow globe, and earthquakes are just someone shaking it up. Area 51 is just a decoy. The real secret government alien research facility is located in a Walmart parking lot.",
-        favoriteConspiracyQuote: `"The real reason the Titanic sank is because it hit an iceberg made of government secrets." - Jesus`,
-        lookingFor: "A good ole farm hand who loves to build legos.",
-    },
-    {
-        name: "Mr. Rainbow Unicorn Sparkly Lights" ,
-        photo: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fthumbs.dreamstime.com%2Fz%2Ffunny-farmer-man-shovel-rake-open-mouthed-expressive-portrait-79253819.jpg&tbnid=CtwiFjdfPSYjsM&vet=12ahUKEwiw3-u2lc3-AhXfjoQIHbd0Am0QMygGegUIARDZAQ..i&imgrefurl=https%3A%2F%2Fwww.dreamstime.com%2Fstock-photo-funny-farmer-man-shovel-rake-open-mouthed-expressive-portrait-image79253819&docid=3tKfUPWKHvOr-M&w=1300&h=957&q=funny%20farmer%20pictures&ved=2ahUKEwiw3-u2lc3-AhXfjoQIHbd0Am0QMygGegUIARDZAQ",
-        theories: "The real reason the Titanic sank is because it hit an iceberg made of government secrets.",
-        favoriteConspiracyQuote: `"I don't trust anyone who says they don't believe in conspiracy theories. It's like saying you don't believe in unicorns." - Neil deGrasse Tyson`,
-        lookingFor: "Someone to love me as much as Casey loves his cat.",
-    }
 ]
